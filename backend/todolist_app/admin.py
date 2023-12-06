a104f236c19869d942e6b3979ee87c3716090691
Arseniy <kichigin_arseniy@mail.ru>
2023-12-06 18:50:10 +0500

    admin update

---
+++ backend/todolist_app/admin.py
@@ -0,0 +1,9 @@
+from .models import *
+from django.contrib import admin
+# Register your models here.
+
+admin.site.register(TaskPerDay)
+admin.site.register(TaskPerWeek)
+admin.site.register(TaskPerMonth)
+admin.site.register(TaskPerYear)
+
