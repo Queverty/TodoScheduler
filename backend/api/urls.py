587fb64d900edc442407651d8df3d70474488542
Arseniy <kichigin_arseniy@mail.ru>
2023-12-07 18:01:45 +0500

    update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import UsersViewSet
+from .views import UsersViewSet, TaskPerDayView, TaskPerWeekView, TaskPerMonthView
 
 router = routers.SimpleRouter()
 router.register(r'users', UsersViewSet, basename='User')
 urlpatterns = [
 	path('', include(router.urls)),
+	path('task-day/', TaskPerDayView.as_view(), ),
+	path('task-week/', TaskPerWeekView.as_view(), ),
+	path('task-month/', TaskPerMonthView.as_view(), ),
 ]
