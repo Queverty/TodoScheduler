c6e0164df05a8b1f29682d75c5651e12ffa2b8b3
Arseniy <kichigin_arseniy@mail.ru>
2023-12-07 17:58:43 +0500

    update views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,72 @@
 from rest_framework import viewsets
-
-from .serializers import UsersSerializer
+from rest_framework.views import APIView
+from .serializers import UsersSerializer, TaskPerDaySerializer, TaskPerWeekSerializer, TaskPerMonthSerializer, \
+	TaskPerMonthCreateSerializer, TaskPerDayCreateSerializer, TaskPerWeekCreateSerializer
 from users.models import User
-
-
+from .services.taskday import TaskPerDayService
+from .services.taskmonth import TaskPerMonthService
+from .services.taskweek import TaskPerWeekService
+from rest_framework.response import Response
 
 
 class UsersViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UsersSerializer
+
+
+class TaskPerDayView(APIView):
+
+	def get(self, request, *args, **kwargs):
+		if request.GET.get('user'):
+			user_id = request.GET.get('user')
+		else:
+			user_id = request.user.id
+		outcome = TaskPerDayService.execute({'user': user_id})
+		serializer = TaskPerDaySerializer(outcome, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, *args, **kwargs):
+		serializer = TaskPerDayCreateSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			serializer.save()
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
+
+
+class TaskPerWeekView(APIView):
+
+	def get(self, request, *args, **kwargs):
+		if request.GET.get('user'):
+			user_id = request.GET.get('user')
+		else:
+			user_id = request.user.id
+		outcome = TaskPerWeekService.execute({'user': user_id})
+		serializer = TaskPerWeekSerializer(outcome, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, *args, **kwargs):
+		serializer = TaskPerWeekCreateSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
+
+
+class TaskPerMonthView(APIView):
+
+	def get(self, request, *args, **kwargs):
+		if request.GET.get('user'):
+			user_id = request.GET.get('user')
+		else:
+			user_id = request.user.id
+		outcome = TaskPerMonthService.execute({'user': user_id})
+		serializer = TaskPerMonthSerializer(outcome, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, *args, **kwargs):
+		serializer = TaskPerMonthCreateSerializer(data=request.data,
+												  context={'user': request.data['user']})
+		if serializer.is_valid(raise_exception=True):
+			serializer.save()
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
--- backend/template_app/views.py
+++ backend/template_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+import json
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 import requests
-
+from django.contrib import messages
 from users.models import User
 
 
@@ -10,11 +13,33 @@ from users.models import User
 
 class IndexView(View):
 
-
 	def get(self, request):
 		if request.user.is_anonymous:
 			return render(request, "template_app/index.html")
-		return render(request, "template_app/index_log.html")
+		day = requests.get('http://localhost:8000/api/task-day/', params={'user': request.user.id}).json()
+		week = requests.get('http://localhost:8000/api/task-week/', params={'user': request.user.id}).json()
+		month = requests.get('http://localhost:8000/api/task-month/', params={'user': request.user.id}).json()
+		return render(request, "template_app/index_log.html",
+					  context={'day': day,
+							   'week': week,
+							   'month': month})
+
+	def post(self, request):
+		task = request.POST.get('Title')
+		description = request.POST.get('Description')
+		if request.POST.get('month'):
+			url = 'http://localhost:8000/api/task-month/'
+		elif request.POST.get('day'):
+			url = 'http://localhost:8000/api/task-day/'
+		elif request.POST.get('week'):
+			url = 'http://localhost:8000/api/task-week/'
+		data = {'title': task, 'description': description, 'user': request.user.id}
+		response = requests.post(url, data=data)
+		data = response.json()
+		if response.status_code != 201:
+			messages.add_message(request, messages.INFO, data)
+		return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 
 class AboutView(View):
